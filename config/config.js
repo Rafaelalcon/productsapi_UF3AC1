aaf3737172c02a7da22115f30023980c6062c509
rafael <rafael.alcon@estudiant.fjaverianas.com>
2024-04-29 20:15:41 +0200

    Variables de entorno

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb+srv://rafaelalcon:eTMEzhQX4BetosvU@products.psdwwve.mongodb.net/?retryWrites=true&w=majority&appName=products/"
+  mongoURI: process.env.mongoURI
 }
